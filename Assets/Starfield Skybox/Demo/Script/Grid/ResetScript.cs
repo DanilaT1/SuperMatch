5b6353ee8f5cc1b0272c7b5b39aa024eac798e89
DanilaT1 <tikhonovdan1la@yandex.ru>
2025-11-25 04:17:29 +0400

    Первая MVP версия для теста
    Обновил данные игрока, чтобы было как при первом запуске и игра начиналась с 1 уровня, можно потестить)

--- Assets/Starfield Skybox/Demo/Script/Grid/ResetScript.cs
+++ Assets/Starfield Skybox/Demo/Script/Grid/ResetScript.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class ResetScript : MonoBehaviour
+public class TestResetter : MonoBehaviour
 {
     void Start()
     {
-        // УДАЛЯЕТ ВСЕ СОХРАНЕНИЯ PLAYERPREFS!
-        PlayerPrefs.DeleteAll();
-        Debug.Log("Все PlayerPrefs сброшены. Можно запускать игру.");
+        // Это будет сбрасывать прогресс при каждом запуске!
+        PlayerPrefs.DeleteAll(); 
+        Debug.Log("Прогресс сброшен автоматически.");
     }
 }
